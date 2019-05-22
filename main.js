6b999ed6e9b49fbd0c9511d0aa75e8625177b7dd
Andy Cho <andrew.cho@plusqa.com>
2019-05-22 08:49:34 -0700

    show initial window

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+
+let win
+
+function createWindow() {
+  // Create the browser window.
+  let win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
