e89e0036d57ba12d1929a27a1e17ad7ece5f6c3e
Andy Cho <andrew.cho@plusqa.com>
2019-05-22 17:35:07 -0700

    output console logs of device id connection status

--- main.js
+++ main.js
@@ -33,3 +33,22 @@ app.on('activate', () => {
     createWindow()
   }
 })
+
+var adb = require('adbkit')
+var client = adb.createClient()
+
+client.trackDevices()
+  .then(function(tracker) {
+    tracker.on('add', function(device) {
+      console.log('Device %s was plugged in', device.id)
+    })
+    tracker.on('remove', function(device) {
+      console.log('Device %s was unplugged', device.id)
+    })
+    tracker.on('end', function() {
+      console.log('Tracking stopped')
+    })
+  })
+  .catch(function(err) {
+    console.error('Something went wrong:', err.stack)
+  })
